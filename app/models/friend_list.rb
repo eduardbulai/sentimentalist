9e9aab8c7500618063f46a18acba1d90ba230a9a
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-23 16:18:48 -0400

    Modify activerecord associations for friend_list model
    has_many :friend_status_messages, :through => :friends

--- app/models/friend_list.rb
+++ app/models/friend_list.rb
@@ -1,6 +1,7 @@
 class FriendList < ActiveRecord::Base
   attr_accessible :id, :name, :user_id
 
-  has_many :friends
   belongs_to :user
+  has_many :friends
+  has_many :friend_status_messages, :through => :friends
 end
