26e9cb5a44af42dcfd3f042762805353ca3d2465
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-23 16:08:27 -0400

    Add activerecord associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :current_city, :email, :first_name, :id, :last_login, :last_name, :password
+
+  has_many :pokes, :user_status_texts, :private_messages
 end
