2956b3399da5c7fb0d3ea3312b4df8a234c79f46
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-23 16:19:22 -0400

    Modify activerecord associations for User model
    has_many :friends, :through => :friend_lists

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :current_city, :email, :first_name, :id, :last_login, :last_name, :password
 
-  has_many :pokes, :user_status_texts, :private_messages
+  has_many :pokes
+  has_many :user_status_texts
+  has_many :private_messages
+
+  has_many :friends, :through => :friend_lists
 end
