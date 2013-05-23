be3e004f563d8c2aa9ce13f4cd388e09a70017a3
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-23 16:58:44 -0400

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ActiveRecord::Base
   has_many :private_messages
 
   has_many :friends, :through => :friend_lists
+
+  validates_format_of :password, :with => /([^a-zA-Z]*([a-zA-Z]+)[^a-zA-Z]*){3,}/, :message => "your password must contain at least 3 alphabetic characters"
+  validates_presence_of :current_city
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :last_login
+  validates_presence_of :email
+  validates_presence_of :password
+  validates_presence_of :id
+
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => "Please enter a valid email address."
+
 end
