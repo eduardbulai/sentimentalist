154f5c53638beaf36bf53312af33b3aca620d683
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-26 21:55:35 -0400

    Add validation to app/models/friend_list.rb

--- app/models/friend_list.rb
+++ app/models/friend_list.rb
@@ -4,4 +4,12 @@ class FriendList < ActiveRecord::Base
   belongs_to :user
   has_many :friends
   has_many :friend_status_messages, :through => :friends
+
+  validates_presence_of :id
+  validates_presence_of :name
+  validates_presence_of :user_id
+
+  validates_format_of :id, :with => Fixnum
+  validates_format_of :name, :with => String
+  validates_format_of :user_id, :with => Fixnum
 end
