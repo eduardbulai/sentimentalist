92a6d9dfb4f225e2ef57e8de6cb0b33a491d25fe
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-26 21:55:16 -0400

    Add validation to app/models/authentication.rb

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,3 +1,13 @@
 class Authentication < ActiveRecord::Base
   attr_accessible :provider, :uid, :user_id
+
+  validates_presence_of :provider
+  validates_presence_of :uid
+  validates_presence_of :user_id
+
+  validates_format_of :provider, :with => String
+  validates_format_of :uid, :with => String
+  validates_numericality_of :uid
+  validates_format_of :user_id, :with => Fixnum
+
 end
