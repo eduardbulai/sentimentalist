34f1a4aa4200c95c575aa5f4074e6753024d51b2
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-26 21:56:24 -0400

    Add validation to app/models/poke.rb

--- app/models/poke.rb
+++ app/models/poke.rb
@@ -2,4 +2,10 @@ class Poke < ActiveRecord::Base
   attr_accessible :id, :user_id
 
   belongs_to :user
+
+  validates_presence_of :id
+  validates_presence_of :user_id
+
+  validates_format_of :id, :with => Fixnum
+  validates_format_of :user_id, :with => Fixnum
 end
