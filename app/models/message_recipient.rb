4aeb1e58aff18d49b426994739dbcfe43364d4ce
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-26 21:56:14 -0400

    Add validation to app/models/message_recipient.rb

--- app/models/message_recipient.rb
+++ app/models/message_recipient.rb
@@ -1,3 +1,11 @@
 class MessageRecipient < ActiveRecord::Base
   attr_accessible :id, :message_id, :user_id
+
+  validates_presence_of :id
+  validates_presence_of :message_id
+  validates_presence_of :user_id
+
+  validates_format_of :id, :with => Fixnum
+  validates_format_of :message_id, :with => Fixnum
+  validates_format_of :user_id, :with => Fixnum
 end
