144f495974a73ed79dadad909d0c6bff9faff621
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-26 21:56:37 -0400

    Add validation to app/models/private_message.rb

--- app/models/private_message.rb
+++ app/models/private_message.rb
@@ -2,4 +2,12 @@ class PrivateMessage < ActiveRecord::Base
   attr_accessible :datetime_sent, :id, :user_id
 
   belongs_to :user
+
+  validates_presence_of :datetime_sent
+  validates_presence_of :id
+  validates_presence_of :user_id
+
+  validates_format_of :datetime_sent, :with => Datetime
+  validates_format_of :id, :with => Fixnum
+  validates_format_of :user_id, :with => Fixnum
 end
