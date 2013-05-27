62e708715c7964705ea3b63ebe8ba2e20ed1f1b4
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-26 21:55:58 -0400

    Add validation to app/models/friend_status_text.rb

--- app/models/friend_status_text.rb
+++ app/models/friend_status_text.rb
@@ -2,4 +2,12 @@ class FriendStatusText < ActiveRecord::Base
   attr_accessible :content, :datetime_posted, :friend_id, :id
 
   belongs_to :friend
+
+  validates_presence_of :content
+  validates_presence_of :datetime_posted
+  validates_presence_of :friend_id
+
+  validates_format_of :datetime_posted, :with => Datetime
+  validates_format_of :friend_id, :with => Fixnum
+  validates_format_of :content, :with => String
 end
