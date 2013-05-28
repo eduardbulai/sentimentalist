16d8e95b46874e457b0ddd5cad3c18f9e180730e
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-28 16:18:53 -0400

    Delete AddRecipientIdColumnToMessage migration

--- db/migrate/20130524171924_add_recipient_id_column_to_message.rb
+++
@@ -1,9 +0,0 @@
-class AddRecipientIdColumnToMessage < ActiveRecord::Migration
-  def up
-  	add_column :private_messages, :recipient_id, :integer
-  end
-
-  def down
-  	remove_column :private_messages, :recipient_id, :integer
-  end
-end
