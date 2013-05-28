58974c0740b1c2ffc88151269b032e889f5261e0
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-28 16:16:56 -0400

    Add schema constraints to FriendList model

--- app/models/friend_list.rb
+++ app/models/friend_list.rb
@@ -1,15 +1,10 @@
 class FriendList < ActiveRecord::Base
-  attr_accessible :id, :name, :user_id
+  attr_accessible :name, :user_id
 
   belongs_to :user
   has_many :friends
   has_many :friend_status_messages, :through => :friends
 
-  validates_presence_of :id
   validates_presence_of :name
   validates_presence_of :user_id
-
-  validates_format_of :id, :with => Fixnum
-  validates_format_of :name, :with => String
-  validates_format_of :user_id, :with => Fixnum
 end
--- db/migrate/20130523195206_create_friend_lists.rb
+++ db/migrate/20130523195206_create_friend_lists.rb
@@ -1,9 +1,8 @@
 class CreateFriendLists < ActiveRecord::Migration
   def change
     create_table :friend_lists do |t|
-      t.integer :id
-      t.string :name
-      t.integer :user_id
+      t.string :name, :NULL => false
+      t.integer :user_id, :NULL => false
 
       t.timestamps
     end
