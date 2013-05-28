62ac7d840381b335da5190d8b623b21717ce3ba4
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-28 16:17:33 -0400

    Add schema constraints to MessageRecipient model

--- app/models/message_recipient.rb
+++ app/models/message_recipient.rb
@@ -1,11 +1,7 @@
 class MessageRecipient < ActiveRecord::Base
-  attr_accessible :id, :message_id, :user_id
+  attr_accessible :message_id
 
-  validates_presence_of :id
+  has_one :message
+
   validates_presence_of :message_id
-  validates_presence_of :user_id
-
-  validates_format_of :id, :with => Fixnum
-  validates_format_of :message_id, :with => Fixnum
-  validates_format_of :user_id, :with => Fixnum
 end
--- db/migrate/20130524171827_create_message_recipients.rb
+++ db/migrate/20130524171827_create_message_recipients.rb
@@ -1,9 +1,7 @@
 class CreateMessageRecipients < ActiveRecord::Migration
   def change
     create_table :message_recipients do |t|
-      t.integer :id
-      t.integer :user_id
-      t.integer :message_id
+      t.integer :message_id, :NULL => false
 
       t.timestamps
     end
