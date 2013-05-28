3fde19027b6de3ff6661c56345679f3196515925
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-28 16:16:21 -0400

    Add schema constraints to Authentication model

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -4,10 +4,4 @@ class Authentication < ActiveRecord::Base
   validates_presence_of :provider
   validates_presence_of :uid
   validates_presence_of :user_id
-
-  validates_format_of :provider, :with => String
-  validates_format_of :uid, :with => String
-  validates_numericality_of :uid
-  validates_format_of :user_id, :with => Fixnum
-
 end
--- db/migrate/20130524171421_create_authentications.rb
+++ db/migrate/20130524171421_create_authentications.rb
@@ -1,9 +1,9 @@
 class CreateAuthentications < ActiveRecord::Migration
   def change
     create_table :authentications do |t|
-      t.string :provider
-      t.string :uid
-      t.integer :user_id
+      t.string :provider, :NULL => false
+      t.string :uid, :NULL => false
+      t.integer :user_id, :NULL => false
 
       t.timestamps
     end
