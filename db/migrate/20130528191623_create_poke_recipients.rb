e10b590c735450b73133ab8cee19a8105354ef07
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-28 16:18:31 -0400

    Add schema constraints to PokeRecipient model

--- db/migrate/20130528191623_create_poke_recipients.rb
+++ db/migrate/20130528191623_create_poke_recipients.rb
@@ -1,6 +1,8 @@
 class CreatePokeRecipients < ActiveRecord::Migration
   def change
     create_table :poke_recipients do |t|
+    	t.integer :user_id, :NULL => false
+    	t.integer :poke_id, :NULL => false
 
       t.timestamps
     end
