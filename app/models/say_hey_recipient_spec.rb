f90f9593d9e6272ed3029d6fedc3c902ce5106bb
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-30 23:25:28 -0400

    Delete misplaced files in app/models directory
    app/models/say_hey_recipient_spec.rb and app/models/say_hey_spec.rb

--- app/models/say_hey_recipient_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe SayHeyRecipient do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- app/models/say_hey_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe SayHay do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
