4e86b152313a62ebf77d0000ca62c853845c6fcd
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-02 18:31:11 -0400

    Add OAuth key and uid to config/initializers/omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  provider :twitter, 'KQvSsA1AFozcJpzQ3EV4w', '1nEZb8ibn1XSxemkeaxQXC8v3MKNqVceOh95XEBTyIE'
 end
