b4ca66671cc57f8106e017a2d2b646200d2f12d3
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-02 20:58:08 -0400

    Modify config/routes.rb to accomodate for omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,10 @@ Sentimentalist::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
+
+  match "/auth/:provider/callback" => "sessions#create"
+  match "/signout" => "sessions#destroy", :as => :signout
 
   # See how all your routes lay out with "rake routes"
 
