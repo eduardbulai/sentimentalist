b289ab9981bc82b6f640f75f69aa95d4b3430165
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-02 21:38:39 -0400

    Modify application_controller.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,13 @@
 class WelcomeController < ApplicationController
 
-	def index
-    @users = User.all
+	# def index
+ #    @users = User.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @users }
-    end
+ #    respond_to do |format|
+ #      format.html # index.html.erb
+ #      format.json { render json: @users }
+ #    end
 
-	end
+	# end
 
 end
