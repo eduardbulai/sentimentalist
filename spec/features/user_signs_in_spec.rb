2b6eb78398c252d31365ee667445768aa4060b3e
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-03 21:30:51 -0400

    Complete user story for user_signs_in_spec.rb

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -1,6 +1,19 @@
 # 	As a returning user
-# 	I want to log in
+# 	I want to sign in
 # 	so I can use the app
-# 	- When a returning user arrives at the index page, they see a “log in” button
-# 	- When a returning user clicks on the “log in” button, they are taken through to their
+# 	- When a returning user arrives at the index page, they see a "sign in” button
+# 	- When a returning user clicks on the sign in” button, they are taken through to their
 # homepage
+
+describe "user signs in" do
+	it "user sees sign in button on welcome page" do
+		visit root_url
+		expect(page).to have_content("Sign In with Twitter")
+	end
+
+	it "user clicks sign in button and is redirected to dashboard" do
+		visit root_url
+		click_link("Sign In with Twitter")
+		assert_redirected_to(dashboard_path)
+	end
+end
