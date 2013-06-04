11dbdc6e9744e8c2e49ef96ed9ab7dd3a12f7fc5
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-03 23:39:36 -0400

    Add specs to spec/features/user_signs_in_spec.rb

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -11,9 +11,21 @@ describe "user signs in" do
 		expect(page).to have_content("Sign In with Twitter")
 	end
 
-	it "user clicks sign in button and is redirected to dashboard" do
+	it "unregistered user clicks sign in button and is redirected to sign in" do
 		visit root_url
 		click_link("Sign In with Twitter")
-		assert_redirected_to(dashboard_path)
+		expect(current_path).to eql('/users/sign_in')
+	end
+
+	it "unregistered user can sign in by inputing their information"
+
+	it "registered user clicks sign in button and is redirected to sign in" do
+		visit root_url
+		click_link("Sign In with Twitter")
+		expect(current_path).to eql('/users/sign_in')
+		fill_in 'Email', :with => 'user@example.com'
+		fill_in 'Password', :with => 'example00'
+		click_button('Sign in')
+		expect(current_path).to eql(dashboard_path)
 	end
 end
