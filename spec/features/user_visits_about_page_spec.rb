f18321e860a59e082a8f9525d9b7920154f1928f
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-03 21:41:21 -0400

    Complete user story for user_visits_about_page_spec.rb

--- spec/features/user_visits_about_page_spec.rb
+++ spec/features/user_visits_about_page_spec.rb
@@ -4,3 +4,20 @@
 
 	# - The welcome page contains a link to a modal window with information about the app
 
+describe "user visits about page" do
+
+	it "user arrives at about page from welcome page" do
+		visit root_url
+		expect(page).to have_content('About')
+		click_link('About')
+		assert_redirected_to(privacy_path)
+	end
+
+	it "user returns to welcome page from about page" do
+		visit privacy_path
+		expect(page).to have_content('Close')
+		click_button("Close")
+		assert_redirected_to(root_url)
+	end
+
+end
