415f112fcdd77de584785aa9118d8990ee1722c8
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-05 09:54:44 -0400

    Remove global configuration from application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,13 +9,4 @@ class ApplicationController < ActionController::Base
   	@current_user||User.find(session[:user_id]) if session[:user_id]
   end
 
-  def client
-	  Twitter.configure do |config|
-		  config.consumer_key = 'KQvSsA1AFozcJpzQ3EV4w'
-		  config.consumer_secret = '1nEZb8ibn1XSxemkeaxQXC8v3MKNqVceOh95XEBTyIE'
-		  config.oauth_token = '823627243-UDZhCacE9mWVMbFprLKq90Rk2sD3iPYujpDXPV4I'
-		  config.oauth_token_secret = 'eJKYJf3IKVRQDTlH59GXWnsrBohMHP49HRrNZA8HXo'
-		end
-  end
-
 end
