29c3a5c58f932c6c10577c78ebc03afc5facbc38
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-05 09:07:17 -0400

    Add failure_path to routes.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,4 +10,18 @@ class SessionsController < ApplicationController
 	  session[:user_id] = nil
 	  redirect_to root_url, :notice => "Signed out!"
 	end
+
+	def show
+    if session['823627243-UDZhCacE9mWVMbFprLKq90Rk2sD3iPYujpDXPV4I'] && session['eJKYJf3IKVRQDTlH59GXWnsrBohMHP49HRrNZA8HXo']
+      @user = client.user(include_entities: true)
+    else
+      redirect_to failure_path
+    end
+  end
+
+  def error
+    flash[:error] = "Sign in with Twitter failed"
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Sentimentalist::Application.routes.draw do
 
   match '/auth/twitter/callback', to: 'sessions#create'
 
-  match "/signout" => "sessions#destroy", :as=>:signout
+  match "/signout", to: "sessions#destroy", as: :signout
   match '/about', to: 'welcome#about'
   match '/privacy', to: 'welcome#privacy'
+  match '/auth/failure', to: 'sessions#error', as: 'failure'
 
   match ':controller(/:action(/:id))(.:format)'
 end
