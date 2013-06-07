7df486613d57d0d6f71abebc21c5c993abb0261f
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-07 15:46:56 -0400

    Add tests to check for emotion icons on dashboard

--- spec/features/user_views_default_dashboard_spec.rb
+++ spec/features/user_views_default_dashboard_spec.rb
@@ -8,16 +8,36 @@ require 'spec_helper'
 # displaying their followers’ emotional statuses
 
 describe "user views default dashboard" do
+
   let(:user) { FactoryGirl.create(:user) }
 
-  it "user sees a dashboard populated by icons displaying their followers emotional statuses"
-
-  it "user sees a navbar listing several time filters" do
+  before(:each) do
   	visit root_url
-  	click_link('Sogn In with Twitter')
+  	click_link('Sign in with Twitter')
+  end
+
+  it "user sees a sidebar listing several time filters" do
   	expect(page).to have_content('Week')
   	expect(page).to have_content('Month')
   	expect(page).to have_content('Year')
+  end
+
+  it "user sees a dashboard populated by icons displaying their followers emotional statuses" do
+  	user = User.last
+  	first_follower = user.followers.first
+  	last_follower = user.followers.last
+  	expect(page).to have_content(first_follower.name)
+  	expect(page).to have_content(last_follower.name)
+  	last_follower.follower_tweets.each do |tweet|
+  		if tweet
+  			expect(page).to have_content(tweet.emotion)
+  		end
+  	end
+  	first_follower.follower_tweets.each do |tweet|
+  		if tweet
+  			expect(page).to have_content(tweet.emotion)
+  		end
+  	end
   end
 
 end
