253717701e984b2ffa8ce9ad009f81be433f4279
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-08 15:34:05 -0400

    Create specs for dashboard filters

--- spec/features/user_filters_dashboard_tweets_spec.rb
+++ spec/features/user_filters_dashboard_tweets_spec.rb
@@ -6,3 +6,35 @@
 # - When a user clicks on a time filter tab, the friend icon field in the center of their
 # dashboard refreshes to display only friends whose statuses have been updated within that timeframe
 # - Timeframes include ‘Today’, 7 Days’, 1 Month’, 1 Year’
+
+describe "user filters dashboard" do
+
+	context "user filters by emotion" do
+
+		it "user views tweets with emotion 'joy' "
+
+		it "user views tweets with emotion 'anger' "
+
+		it "user views tweets with emotion 'sadness' "
+
+		it "user views tweets with emotion 'surprise' "
+
+		it "user views tweets with emotion 'disgust' "
+
+		it "user views tweets with emotion 'fear' "
+
+		it "user views tweets with emotion 'other' "
+
+	end
+
+	context "user filters by timeframe" do
+
+		it "user views tweets from last week"
+
+		it "user views tweets from last month"
+
+		it "user views tweets for the year"
+
+	end
+
+end
