2f796c6d983b941a7d1600db73a153d6012b0e9d
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-09 23:44:56 -0400

    Delete unnecessary stylesheets and migrations

--- db/migrate/20130607020536_add_emotion_to_follower_tweets.rb
+++
@@ -1,9 +0,0 @@
-class AddEmotionToFollowerTweets < ActiveRecord::Migration
-  def up
-  	add_column :follower_tweets, :emotion, :string
-  end
-
-  def down
-  	remove_column :follower_tweets, :emotion
-  end
-end
--- db/migrate/20130607020801_add_polarity_to_follower_tweets.rb
+++
@@ -1,9 +0,0 @@
-class AddPolarityToFollowerTweets < ActiveRecord::Migration
-  def up
-  	add_column :follower_tweets, :polarity, :integer
-  end
-
-  def down
-  	remove_column :follower_tweets, :polarity
-  end
-end
