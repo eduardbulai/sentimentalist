23ea0d345d29b12ae93c908d10e61ae6b2c7ffec
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-13 00:15:21 -0400

    Add post_to_twitter method to dashboard_controller.rb

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,9 +4,14 @@ class DashboardController < ApplicationController
 		@followers = current_user.followers
 
 		respond_to do |format|
-      format.html # index.html.erb
+      format.html
       format.json { render json: @user_tweets }
     end
+
+  def post_to_twitter
+  	tweeter = current_user.tweeter
+  	tweeter.update(params[:tweet])
+  end
 
 	end
 end
