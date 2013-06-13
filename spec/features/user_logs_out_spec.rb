a4f688db84de03364647496c89c07debb75241af
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-13 08:15:38 -0400

    Complete user_signs_out user story

--- spec/features/user_logs_out_spec.rb
+++
@@ -1,6 +0,0 @@
-	# As a user
-	# I want to log out
-	# So I can leave the app
-
-	# - When a user clicks the logout button, their session ends
-	# - When a user clicks the logout button, they are redirected to the welcome page
