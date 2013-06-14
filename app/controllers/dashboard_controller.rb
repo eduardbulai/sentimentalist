d5ac8227624aa725db96c3144fbe536a4f6195b2
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-14 00:16:46 -0400

    Add post to twitter alert to dashboard_controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -11,6 +11,8 @@ class DashboardController < ApplicationController
   def post_to_twitter
   	tweeter = current_user.tweeter
   	tweeter.update(params[:tweet])
+  	flash[:alert] = "Your Tweet has been Posted"
+  	redirect_to dashboard_index_path
   end
 
 	end
