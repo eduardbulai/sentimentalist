72d152df8bcbe9ad1874582bb5022b374b75a593
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-17 23:04:00 -0400

    Delete unused tweet hashtag models

--- app/models/follower_tweet_hashtag.rb
+++
@@ -1,7 +0,0 @@
-class FollowerTweetHashtag < ActiveRecord::Base
-  attr_accessible :tag, :follower_tweet_id
-
-  belongs_to :follower_tweet
-
-  validates_presence_of :tag
-end
--- spec/models/follower_tweet_hashtag_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe FollowerTweetHashtag do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
