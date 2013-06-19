ef0b6198c9f6c1fe2f81bf23e9a141d2f0556a18
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-19 17:52:23 -0400

    Modify user_tweets_modal_functionality

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class DashboardController < ApplicationController
-	def index
+
+  respond_to :json, :html
+
+  def index
 
 		@followers = current_user.followers
     @user_tweets = current_user.user_tweets
@@ -9,12 +12,25 @@ class DashboardController < ApplicationController
       format.json { render json: @user_tweets }
     end
 
-  def post_to_twitter
-  	tweeter = current_user.tweeter
-  	tweeter.update(params[:tweet])
-  	flash[:alert] = "Your Tweet has been Posted"
-  	redirect_to dashboard_index_path
-  end
-
 	end
+
+  def post_to_twitter
+      tweeter = current_user.tweeter
+      tweeter.update(params[:tweet])
+      flash[:alert] = "Your Tweet has been Posted"
+      redirect_to dashboard_index_path
+    end
+
+    def update_machine_learner
+      #params(emotion)
+      emotion = params[:emotion]
+      tweet = UserTweet.find(params[:id])
+      tweet.emotion = emotion
+      if tweet.save
+        render :json => [tweet]
+      else
+        render :json => [], :status => :unprocessable_entity
+      end
+    end
+
 end
