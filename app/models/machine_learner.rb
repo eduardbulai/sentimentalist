a76b47a5e63d163c9dc1067c1f9f808774e7d0eb
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-23 11:09:38 -0400

    Add machine_learner_is_trained method to MachineLearner plus relevant specs

--- app/models/machine_learner.rb
+++ app/models/machine_learner.rb
@@ -12,7 +12,8 @@ class MachineLearner < ActiveRecord::Base
   	machine_learner.ignore_words = Stopwords.stopwords
   	machine_learner.stemming = true
   	machine_learner.wcount = {}
-  	machine_learner.ccount = {}
+  	machine_learner.ccount = {anger: 0, joy: 0, fear: 0, sadness: 0, disgust: 0, surprise: 0, ambiguous: 0}
+    machine_learner.trained = nil
   end
 
   def build_classifier
@@ -21,13 +22,28 @@ class MachineLearner < ActiveRecord::Base
     new_classifier.instance_variable_set(:@ccount, self.ccount)
     new_classifier.instance_variable_set(:@ignore_words, self.ignore_words)
     new_classifier.instance_variable_set(:@stemming, self.stemming)
+    new_classifier.instance_variable_set(:@trained, self.trained)
     new_classifier
   end
 
   def persist_machine_learner new_classifier
     self.wcount = new_classifier.instance_variable_get(:@wcount)
     self.ccount = new_classifier.instance_variable_get(:@ccount)
+    if !self.trained
+      self.trained = self.machine_learner_is_trained
+    end
     self.save
+  end
+
+  def machine_learner_is_trained
+    trained = true
+    emotions = [:anger, :joy, :fear, :sadness, :disgust, :surprise, :ambiguous]
+    emotions.each do |emotion|
+      if self.ccount[emotion] < 15
+        trained = false
+      end
+    end
+    trained
   end
 
 end
--- spec/models/machine_learner_spec.rb
+++ spec/models/machine_learner_spec.rb
@@ -28,8 +28,12 @@ describe MachineLearner do
     	expect(machine_learner.wcount).to eql({})
     end
 
-    it "ccount is an empty hash" do
-    	expect(machine_learner.ccount).to eql({})
+    it "ccount is a non-empty hash" do
+    	expect(machine_learner.ccount).to eql({anger: 0, joy: 0, fear: 0, sadness: 0, disgust: 0, surprise: 0, ambiguous: 0})
+    end
+
+    it "trained is set to nil" do
+      expect(machine_learner.trained).to be_nil
     end
 
     it "can push items to wcount" do
@@ -40,6 +44,33 @@ describe MachineLearner do
     it "can push items to ccount" do
     	machine_learner.ccount[:dog] = 1
     	expect(machine_learner.ccount).to_not be_empty
+    end
+
+  end
+
+  context "methods" do
+
+    describe "#machine_learner_is_trained" do
+
+      it 'returns false if machine learner has not been trained' do
+          expect(machine_learner.machine_learner_is_trained).to be_false
+      end
+
+      it 'returns true if machine learner has been trained' do
+
+        expect(machine_learner.machine_learner_is_trained).to be_false
+
+        emotions = [:anger, :joy, :fear, :sadness, :disgust, :surprise, :ambiguous]
+        emotions.each do |emotion|
+          15.times do
+            machine_learner.ccount[emotion]+=1
+          end
+        end
+
+        expect(machine_learner.machine_learner_is_trained).to be_true
+
+      end
+
     end
 
   end
