d446a83d89de2cca528236e49d8bc32f2a2e4ff6
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-25 16:19:44 -0400

    Remove [Rails.env.to_sym] from redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,7 +3,7 @@ configatron.redis_url = {
   :test => "localhost:6379",
   :staging => ENV['REDISTOGO_URL'],
   :production => ENV['REDISTOGO_URL']
-}[Rails.env.to_sym]
+}
 
 uri = URI.parse(configatron.redis_url)
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
