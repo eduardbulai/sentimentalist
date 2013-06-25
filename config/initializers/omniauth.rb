0ea10500b0e9b0242d736d37af3a76721d3f102d
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-25 15:41:53 -0400

    Fix bug in config/initializers/omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, configatron.twitter_key, configatron.twitter_secret
   provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
 end
