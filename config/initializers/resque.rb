19e940d41f9496bd020014b3fd4f90e09bc9bcba
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-26 11:24:22 -0400

    Modify resque.rb

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -6,11 +6,3 @@ Resque.redis = $redis
 
 # configure the schedule
 Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
-
-
-task "resque:setup" => :environment do
-  ENV['QUEUE'] = '*'
-end
-
-desc "Alias for resque:work (To run workers on Heroku)"
-task "jobs:work" => "resque:work"
