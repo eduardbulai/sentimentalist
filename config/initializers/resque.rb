e180bd5320bcaa98d0e885e48fbbb2b5afad47bc
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-26 11:15:32 -0400

    Add heroku script to resque.rb

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,7 +1,16 @@
 require 'resque_scheduler'
+require 'resque/tasks'
 
 # configure redis connection
 Resque.redis = $redis
 
 # configure the schedule
 Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
+
+
+task "resque:setup" => :environment do
+  ENV['QUEUE'] = '*'
+end
+
+desc "Alias for resque:work (To run workers on Heroku)"
+task "jobs:work" => "resque:work"
