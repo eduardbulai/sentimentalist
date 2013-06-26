ccc47031277f84460a6a2de7ad9e14efe5c4bd4c
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-26 14:19:18 -0400

    Modify procfile with resque-heroku gem

--- Procfile
+++ Procfile
@@ -1,5 +1,5 @@
 web: bundle exec rails server thin -p $PORT
-worker: env QUEUE=* bundle exec rake resque:work
+worker: bundle exec rake jobs:work
 scheduler: bundle exec rake resque:scheduler
 
 
--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,8 +1,8 @@
-# require 'resque_scheduler'
-# require 'resque/plugins/heroku'
+require 'resque_scheduler'
+require 'resque/plugins/heroku'
 
-# # configure redis connection
-# Resque.redis = $redis
+# configure redis connection
+Resque.redis = $redis
 
-# # configure the schedule
-# Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
+# configure the schedule
+Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
