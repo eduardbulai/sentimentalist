0281b172c2ec36b849d6c858bb62cbe0a588436d
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-26 10:43:08 -0400

    Add redis:// to development uri

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,13 +1,10 @@
-# configatron.redis_url = {
-#   :development => "localhost:6379",
-#   :staging => ENV['REDISTOGO_URL'],
-#   :production => ENV['REDISTOGO_URL']
-# }[Rails.env.to_sym]
+configatron.redis_url = {
+  :development => "redis://localhost:6379",
+  :staging => ENV['REDISTOGO_URL'],
+  :production => ENV['REDISTOGO_URL']
+}[Rails.env.to_sym]
 
-# uri = URI.parse(configatron.redis_url)
-# $redis = Redis.new(:host => uri.host.to_s, :port => uri.port.to_s, :password => uri.password.to_s)
-# Resque.redis = $redis
+uri = URI.parse(configatron.redis_url)
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Resque.redis = $redis
 
-
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host.to_s, :port => uri.port.to_s, :password => uri.password.to_s)
