19c70891efe7d591735cfebb664c988260849a1a
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-27 19:06:35 -0400

    Marginal progress on views

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,14 +1,14 @@
-require 'resque_scheduler'
+# require 'resque_scheduler'
 
-# configatron.redis_url = {
-#   :development => "redis://localhost:6379",
-#   :staging => ENV['REDISTOGO_URL'],
-#   :production => ENV['REDISTOGO_URL']
-# }[Rails.env.to_sym]
+configatron.redis_url = {
+  :development => "redis://localhost:6379",
+  :staging => ENV['REDISTOGO_URL'],
+  :production => ENV['REDISTOGO_URL']
+}[Rails.env.to_sym]
 
-# uri = URI.parse(configatron.redis_url)
+uri = URI.parse(configatron.redis_url)
 
-# # configure redis connection
-# Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# configure redis connection
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 # configure the schedule
 Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
