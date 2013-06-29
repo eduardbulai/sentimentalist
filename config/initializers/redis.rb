e9f4bc841b414455194b0afe0e616a454557ede4
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-29 11:05:51 -0400

    Uncomment redis.rb and resque.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,12 +1,10 @@
-# configatron.redis_url = {
-#   :development => "redis://localhost:6379",
-#   :test => "redis://localhost:6379",
-#   :staging => ENV['REDISTOGO_URL'],
-#   :production => ENV['REDISTOGO_URL']
-# }[Rails.env.to_sym]
+configatron.redis_url = {
+  :development => "redis://localhost:6379",
+  :staging => ENV['REDISTOGO_URL'],
+  :production => ENV['REDISTOGO_URL']
+}[Rails.env.to_sym]
 
 
-# uri = URI.parse(configatron.redis_url)
-# $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-# Resque.redis = $redis
+uri = URI.parse(configatron.redis_url)
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,14 +1,6 @@
 require 'resque_scheduler'
 
-# configatron.redis_url = {
-#   :development => "redis://localhost:6379",
-#   :staging => ENV['REDISTOGO_URL'],
-#   :production => ENV['REDISTOGO_URL']
-# }[Rails.env.to_sym]
-
-# uri = URI.parse(configatron.redis_url)
-
-# # configure redis connection
-# Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# configure redis connection
+Resque.redis = $redis
 # configure the schedule
 Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
