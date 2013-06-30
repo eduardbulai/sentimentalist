ac315ae9e24b8f72f414e56c14bddc320b3000a9
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-30 14:14:47 -0400

    Add clean_tweet and update_emotions methods to user_tweet.rb

--- app/models/user_tweet.rb
+++ app/models/user_tweet.rb
@@ -12,6 +12,18 @@ class UserTweet < ActiveRecord::Base
     :user_id,
     :tweet_id
 
+
+    def clean_tweet
+      tweet = self.text
+      tweet.downcase!
+      tweet.gsub!(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/, '')
+      tweet.gsub!(/(?=\w*h)(?=\w*t)(?=\w*t)(?=\w*p)\w*/, '')
+      tweet.gsub!(/\s\s+/,' ')
+    end
+
+    def update_emotions(new_emotion)
+      self.emotion = new_emotion
+      self.bayesian_emotion = new_emotion
+    end
+
 end
-
-
