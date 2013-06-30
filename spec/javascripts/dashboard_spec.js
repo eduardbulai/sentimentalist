2055de08770358f290c7a041af9828eefa060299
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-30 00:17:28 -0400

    Create specs and accompanying fixtures for dashboard and load javascript using jasmine

--- spec/javascripts/dashboard_spec.js
+++ spec/javascripts/dashboard_spec.js
@@ -12,7 +12,9 @@ describe('updateBayesianProfileIcon()', function() {
 
   it("renders new elements in the DOM", function() {
 
-    loadFixtures("dashboard.html");
+    loadFixtures("dashboard_user_profile_icon.html");
+
+    expect($('dl')).toHaveClass('palette-anger');
 
     var stuff = {'user': { 'bayesian_emotion': 'surprise' } };
 
@@ -26,16 +28,71 @@ describe('updateBayesianProfileIcon()', function() {
 
 });
 
-// describe("setNewIcon(data)", function() {
 
-//   it("makes an ajax request to the correct URL", function() {
+describe("change user_tweet emotion icon via modal", function() {
 
-//     loadFixtures("user_assessment_modal.html");
+  beforeEach(function () {
+    loadFixtures("dashboard_emotion_icon.html");
+  });
 
-//     spyOn($, "ajax");
-//     $('.user-assessment-modal a').trigger('click');
-//     expect($.ajax).toHaveBeenCalledWith("dashboard/update_machine_learner", jasmine.any(Function));
-//   });
+  it("click triggers ajax request to the correct URL", function() {
 
-// });
+    var id = 18;
+    var text = "surprise";
+    var newEmotion = "surprise";
 
+    spyOn($, "ajax");
+    updateMachineLearner(text, id, newEmotion);
+    expect($.ajax).wasCalled();
+
+  });
+
+  it("click changes icon color", function() {
+
+    var fakeResponse = [{"id": 18},{"bayesian_emotion": "disgust"},{"created_at": "2013-06-29T16:23:03Z"},
+      {"emotion": "disgust"},{"polarity": 7},{"text": "this is a tweet"},
+      {"tweet_id": "337283249111969792"},{"updated_at": "2013-06-30T01:13:22Z"},
+      {"user_id": 1}];
+    var id = 18;
+    var text = "surprise";
+    var newEmotion = "surprise";
+
+    expect($('dl#iconcolor18')).toHaveClass('palette-ambiguous');
+    expect($('dl#iconcolor_bayes18')).toHaveClass('palette-anger');
+
+    spyOn($, "ajax").andCallFake(function(params) {
+      params.success(fakeResponse);
+    });
+    updateMachineLearner(text, id, newEmotion);
+
+    expect($('dl#iconcolor18')).toHaveClass('palette-surprise');
+    expect($('dl#iconcolor_bayes18')).toHaveClass('palette-surprise');
+
+  });
+
+
+  it("click changes icon text", function() {
+
+    var fakeResponse = [{"id": 18},{"bayesian_emotion": "disgust"},{"created_at": "2013-06-29T16:23:03Z"},
+      {"emotion": "disgust"},{"polarity": 7},{"text": "this is a tweet"},
+      {"tweet_id": "337283249111969792"},{"updated_at": "2013-06-30T01:13:22Z"},
+      {"user_id": 1}];
+    var text = "surprise";
+    var id = 18;
+    var newEmotion = "surprise";
+
+    expect($('dd#icontext18')).toHaveText('ambiguous');
+    expect($('dd#icontext_bayes18')).toHaveText('anger');
+
+    spyOn($, 'ajax').andCallFake(function(params) {
+      params.success(fakeResponse);
+    });
+    updateMachineLearner(text, id, newEmotion);
+
+    expect($('#icontext18')).toHaveText('surprise');
+    expect($('#icontext_bayes18')).toHaveText('surprise');
+
+  });
+
+
+});
