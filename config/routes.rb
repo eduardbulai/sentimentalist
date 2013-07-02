3c46e0f4aa2e02b966b8367b3570ce106e4c8f85
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-07-02 09:36:53 -0400

    Restrict resources for :welcome

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Sentimentalist::Application.routes.draw do
   mount Resque::Server, at: '/resque'
 
   resources :dashboard, only: [:index, :load]
-  resources :welcome
+  resources :welcome, only: [:index]
 
   match '/auth/twitter/callback', to: 'sessions#create'
 
