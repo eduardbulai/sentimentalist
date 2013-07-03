0ece8f63a23df3326b4047f27e16166695988843
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-07-03 16:46:49 -0400

    Constrain Api::version1 to not need active session

--- app/controllers/api/version1/dashboard_controller.rb
+++ app/controllers/api/version1/dashboard_controller.rb
@@ -2,10 +2,12 @@ module Api
   module Version1
     class DashboardController < ApplicationController
       def index
-        @user = current_user
-        @followers = current_user.followers
-        @user_tweets = current_user.user_tweets
-        render json: {user: @user, user_tweets: @user_tweets, followers: @followers}
+      #   @user = current_user
+      #   @followers = current_user.followers
+      #   @user_tweets = current_user.user_tweets
+      #   render json: {user: @user, user_tweets: @user_tweets, followers: @followers}
+        @user = User.last
+        render json: {user: @user}
       end
 
       def load
