b8d4307590d86a9af8c3f29cd365d3b4e8089940
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-07-04 14:49:57 -0400

    Refactor api for test

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -2,10 +2,12 @@ module Api
   module V1
     class DashboardController < ApplicationController
       def index
-        @user = current_user
-        @followers = current_user.followers
-        @user_tweets = current_user.user_tweets
-        render json: {user: @user, user_tweets: @user_tweets, followers: @followers}
+        # @user = current_user
+        # @followers = current_user.followers
+        # @user_tweets = current_user.user_tweets
+        # render json: {user: @user, user_tweets: @user_tweets, followers: @followers}
+        @user = User.last
+        render json: {name: @user.name}
       end
 
       def load
